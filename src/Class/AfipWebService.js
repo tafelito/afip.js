0985563a2b26b551997ee6abda4bacb32acf1206
Jonathan Tafel <tafelito@gmail.com>
2022-05-27 14:53:53 -0400

    fix namespaceArrayElements init option

--- src/Class/AfipWebService.js
+++ src/Class/AfipWebService.js
@@ -3,13 +3,13 @@ const path = require('path');
 
 /**
  * Base class for AFIP web services 
- **/ 
+ **/
 module.exports = class AfipWebService {
-	constructor(webServiceOptions){
+	constructor(webServiceOptions) {
 		if (!webServiceOptions) {
 			throw new Error('Missing Web Service Object');
 		}
-		
+
 		/**
 		 * Force to use SOAP Client version 1.2
 		 *
@@ -22,7 +22,7 @@ module.exports = class AfipWebService {
 		 *
 		 * @var boolean
 		 **/
-		 this.nsArrayElems = webServiceOptions.nsArrayElems || true;
+		this.nsArrayElems = typeof webServiceOptions.nsArrayElems !== 'undefined' ? webServiceOptions.nsArrayElems : true;
 
 		/**
 		 * File name for the Web Services Description Language
@@ -30,7 +30,7 @@ module.exports = class AfipWebService {
 		 * @var string
 		 **/
 		this.WSDL = webServiceOptions.WSDL;
-		
+
 		/**
 		 * The url to web service
 		 *
@@ -52,18 +52,18 @@ module.exports = class AfipWebService {
 		 * @var string
 		 **/
 		this.URL_TEST = webServiceOptions.URL_TEST;
-		
+
 		/**
 		 * The Afip parent Class
 		 *
 		 * @var Afip
 		 **/
 		this.afip = webServiceOptions.afip;
-		
+
 		if (this.afip.options['production']) {
 			this.WSDL = path.resolve(__dirname, '../Afip_res', this.WSDL);
 		}
-		else{
+		else {
 			this.WSDL = path.resolve(__dirname, '../Afip_res', this.WSDL_TEST);
 			this.URL = this.URL_TEST;
 		}
@@ -78,27 +78,27 @@ module.exports = class AfipWebService {
 	async executeRequest(operation, params = {}, options) {
 		// Create SOAP client
 		if (!this.soapClient) {
-			let soapClientOptions = { 
-				disableCache: true, 
+			let soapClientOptions = {
+				disableCache: true,
 				forceSoap12Headers: this.soapv12,
 				namespaceArrayElements: this.nsArrayElems,
 				customDeserializer: {
 					// this function will be used to any date found in soap responses
 					date: function (text, context) {
-					  /* text is the value of the xml element.
-						context contains the name of the xml element and other infos :
-						  {
-							  name: 'lastUpdatedDate',
-							  object: {},
-							  schema: 'xsd:date',
-							  id: undefined,
-							  nil: false
-						  }
-		  
-					   */
-					  return text;
+						/* text is the value of the xml element.
+						  context contains the name of the xml element and other infos :
+							{
+								name: 'lastUpdatedDate',
+								object: {},
+								schema: 'xsd:date',
+								id: undefined,
+								nil: false
+							}
+		    
+						 */
+						return text;
 					}
-				  }
+				}
 			};
 
 			this.soapClient = await soap.createClientAsync(this.WSDL, soapClientOptions);
@@ -107,8 +107,8 @@ module.exports = class AfipWebService {
 		}
 
 		// Call to SOAP method
-		let [ result ] = await this.soapClient[operation+'Async'](params, options);
-		
+		let [result] = await this.soapClient[operation + 'Async'](params, options);
+
 		//Return response parsed as JSON
 		return result;
 	}
